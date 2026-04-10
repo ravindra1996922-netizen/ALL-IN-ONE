19dded115c872d75d22fc0a2ad68dfd97f62b7cb
maverick <ravindra1996922@gmail.com>
2026-04-10 12:04:24 +0530

    issue solved

--- allinone/src/api/api_builder.js
+++ allinone/src/api/api_builder.js
@@ -1,3 +1,5 @@
+import { END_POINTS } from "../features/signup_and_login_feature/api/endpointsConstant";
+
 export function buildURL(path) {
   const baseURL = END_POINTS.baseUrl;
   return new URL(path, baseURL).toString();
