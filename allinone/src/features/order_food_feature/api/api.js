9488b7ed305943a0825807aad6361fb876a5d520
maverick <ravindra1996922@gmail.com>
2026-04-11 00:04:57 +0530

    chore: update order food API

--- allinone/src/features/order_food_feature/api/api.js
+++ allinone/src/features/order_food_feature/api/api.js
@@ -1,34 +1,29 @@
-import { FOODS_ENDPOINTS } from "./foods_endpoint"
+import { buildURL } from "../../../utils/api/api_builder";
+import { FOODS_ENDPOINTS } from "./foods_endpoint";
 
-import { buildURL } from "../../../api/api_builder";
-// Food
 export async function fetchFoods(page) {
-    const foods = FOODS_ENDPOINTS.foods;
-   const url =  buildURL(foods);
-   url.searchParams.set("_page", page);
-   url.searchParams.set("_limit", 30);
-   try {
-const foodsResponse = await fetch(url);
-   const foodsData = await foodsResponse.json();
-   return foodsData;
-//    console.log(foodsData.length);
-   }
-   catch(error) {}
-   
+  const foods = FOODS_ENDPOINTS.foods;
+  const url = buildURL(foods);
+  url.searchParams.set("_page", page);
+  url.searchParams.set("_limit", 30);
+  try {
+    const foodsResponse = await fetch(url);
+    const foodsData = await foodsResponse.json();
+    return foodsData;
+  } catch (error) {}
 }
- 
+
 // Recipe
 export async function fetchRecipes(page) {
-        const recipes = FOODS_ENDPOINTS.recipes;
+  const recipes = FOODS_ENDPOINTS.recipes;
 
-     const url = buildURL(recipes);
-     url.searchParams.set("_page", page);
-     url.searchParams.set("_limit", 30);
-     try{
-     const recipesResponse = await fetch(url);
-     const recipesData = await recipesResponse.json();
-     return recipesData;
-     console.log(recipesData.length);
-     }
-     catch(error) {}
+  const url = buildURL(recipes);
+  url.searchParams.set("_page", page);
+  url.searchParams.set("_limit", 30);
+  try {
+    const recipesResponse = await fetch(url);
+    const recipesData = await recipesResponse.json();
+    return recipesData;
+    console.log(recipesData.length);
+  } catch (error) {}
 }
